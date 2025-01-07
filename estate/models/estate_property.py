af00b33bedcf0e7ffa61350b80ad2f9ae6bde7c8
VCombier <vcr@easi-soft.fr>
2025-01-07 16:56:28 +0100

    chap 8

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from odoo import models, fields
+from odoo import models, fields,api
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -32,7 +32,7 @@ class EstateProperty(models.Model):
         ('offer_received', 'Offer Received'),
         ('offer_accepted', 'Offer Accepted'),
         ('sold', 'Sold'),
-        ('cancelled', 'Cancelled'),
+        ('canceled', 'Canceled'),
     ], string='State', required=True, default='new', copy=False)
 
     property_type_id = fields.Many2one('estate.property.type', string="Property Type", required=True)
@@ -44,3 +44,36 @@ class EstateProperty(models.Model):
     offer_ids = fields.One2many(
         'estate.property.offer', 'property_id', string="Offers"
     )
+    best_price = fields.Float(string='Best Offer', compute="_compute_best_price", store=True)
+    total_area = fields.Float(string="Total Area", compute="_compute_total_area", store=True)
+
+    @api.depends('living_area','garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + (record.garden_area if record.garden_area else 0)
+
+    @api.depends('offer_ids')
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'), default=0)
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
+
+    def action_sold(self):
+        for record in self:
+            if record.state == 'canceled':
+                raise ValueError("Canceled")
+            record.state = 'sold'
+
+    def action_cancel(self):
+        for record in self:
+            if record.state == 'sold':
+                raise ValueError("Sold")
+            record.state = 'canceled'
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+from datetime import timedelta
+
+from odoo import models, fields, api
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -11,3 +13,40 @@ class EstatePropertyOffer(models.Model):
 
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
+
+    validity = fields.Integer(string='Validity', default=7)
+    date_deadline = fields.Date(
+        string='Deadline',
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+        store=True,
+    )
+
+    @api.depends('create_date','validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(days=record.validity)
+            else:
+                record.date_deadline = None
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date and record.date_deadline:
+                delta = (record.date_deadline - record.create_date.date()).days
+                record.validity = delta
+            else:
+                record.validity = 0
+
+    def action_accept(self):
+        for offer in self:
+            offer.status = 'accepted'
+            offer.property_id.state = 'offer_accepted'
+            offer.property_id.buyer_id = offer.partner_id
+            offer.property_id.selling_price = offer.price
+            other_offers = offer.property_id.offer_ids - offer
+            other_offers.write({'status': 'refused'})
+
+    def action_refuse(self):
+        for offer in self:
+            offer.status = 'refused'
